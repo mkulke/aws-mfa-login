66a4b06446195452716451d7f3eb67408a7b3069
Magnus Kulke <mkulke@gmail.com>
2021-12-30 13:47:18 +0100

    Reorganize into fn's

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use figment::Figment;
 use serde::{Deserialize, Serialize};
 use std::convert::{TryFrom, TryInto};
 use std::error::Error;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
@@ -56,49 +56,44 @@ impl TryFrom<AssumeRoleOutput> for Credentials {
     }
 }
 
-fn set_credentials(ini: &mut Ini, profile: &str, credentials: Credentials) {
-    ini.set(
-        profile,
-        "aws_access_key_id",
-        Some(credentials.access_key_id),
-    );
-    ini.set(
-        profile,
-        "aws_secret_access_key",
-        Some(credentials.secret_access_key),
-    );
-    ini.set(
-        profile,
-        "aws_session_token",
-        Some(credentials.session_token),
-    );
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let Opt { token } = Opt::from_args();
-
-    let home_dir = dirs::home_dir().ok_or("could not get home directory")?;
-    let config_path: PathBuf = [home_dir.clone(), format!(".{}.toml", PKG_NAME).into()]
+fn set_credentials(
+    config: &Config,
+    home_dir: &Path,
+    credentials: Credentials,
+) -> Result<(), Box<dyn Error>> {
+    let ini_path: PathBuf = [home_dir.to_path_buf(), ".aws".into(), "credentials".into()]
         .iter()
         .collect();
-    let Config {
-        role_arn,
-        mfa_serial_number,
-        session_name,
-        aws_profile,
-    } = Figment::new().merge(Toml::file(&config_path)).extract()
-        .map_err(|e| {
-            format!("{}: {}", &config_path.to_string_lossy(), e)
-        })?;
+    let mut ini = Ini::new();
+    ini.set_default_section("");
+    ini.load(&ini_path)?;
 
+    let Config { aws_profile, .. } = config;
+
+    let mut set = |k, v| ini.set(aws_profile, k, Some(v));
+    set("aws_access_key_id", credentials.access_key_id);
+    set("aws_secret_access_key", credentials.secret_access_key);
+    set("aws_session_token", credentials.session_token);
+    ini.write(&ini_path)?;
+
+    Ok(())
+}
+
+async fn assume_role(config: &Config, token: &str) -> Result<Credentials, Box<dyn Error>> {
     let region = RegionProviderChain::default_provider()
         .region()
         .await
         .ok_or("default region unset")?;
 
-    let config = aws_config::from_env().region(region).load().await;
-    let client = Client::new(&config);
+    let client_config = aws_config::from_env().region(region).load().await;
+    let client = Client::new(&client_config);
+
+    let Config {
+        role_arn,
+        mfa_serial_number,
+        session_name,
+        ..
+    } = config;
 
     let assume_role_output = client
         .assume_role()
@@ -116,16 +111,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             String::from("unknown error")
         })?;
+
     let credentials: Credentials = assume_role_output.try_into()?;
 
-    let mut credentials_ini = Ini::new();
-    let credentials_path: PathBuf = [home_dir, ".aws".into(), "credentials".into()]
+    Ok(credentials)
+}
+
+fn get_config(home_dir: &Path) -> Result<Config, Box<dyn Error>> {
+    let config_path: PathBuf = [home_dir.to_path_buf(), format!(".{}.toml", PKG_NAME).into()]
         .iter()
         .collect();
-    credentials_ini.set_default_section("");
-    credentials_ini.load(&credentials_path)?;
-    set_credentials(&mut credentials_ini, &aws_profile, credentials);
-    credentials_ini.write(&credentials_path)?;
+
+    let config: Config = Figment::new()
+        .merge(Toml::file(&config_path))
+        .extract()
+        .map_err(|e| format!("{}: {}", &config_path.to_string_lossy(), e))?;
+
+    Ok(config)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let Opt { token } = Opt::from_args();
+
+    let home_dir = dirs::home_dir().ok_or("could not resolve home directory")?;
+    let config = get_config(&home_dir)?;
+    let credentials = assume_role(&config, &token).await?;
+    set_credentials(&config, &home_dir, credentials)?;
 
     Ok(())
 }
